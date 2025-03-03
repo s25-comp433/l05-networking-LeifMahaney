0708169a02886b618797770f8c648868f27bad37
Leif Mahaney <leleif@ad.unc.edu>
2025-03-02 22:47:25 -0500

    Video

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -1,21 +1,57 @@
-//
-//  ContentView.swift
-//  BasketballGames
-//
 //  Created by Samuel Shi on 2/27/25.
 //
 
 import SwiftUI
 
+struct Response: Codable{
+    var games: [Game]
+}
+struct Score: Codable, Hashable{
+    var unc: Int
+    var opponent: Int
+}
+
+struct Game: Codable, Identifiable{
+    var id: Int
+    var team: String
+    var opponent: String
+    var date: String
+    var score: Score
+    var isHomeGame: Bool
+    var scoreSummary: String{
+        "Score: UNC \(score.unc) - \(score.opponent) \(opponent)"
+    }
+}
+
 struct ContentView: View {
+    @State private var games = [Game]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(games){game in
+                VStack(alignment: .leading){
+                    Text("\(game.team) Basketball vs. \(game.opponent)")
+                        .font(.headline)
+                    Text(game.scoreSummary)
+                    Text("Date: \(game.date)")
+                }
+            }
+            .task{
+                await loadData()
+    }
+}
+    func loadData() async{
+        guard let url = URL(string: "https://api.samuelshi.com/uncbasketball")else{
+            print("Invalid URL")
+            return
         }
-        .padding()
+        do{
+            let(data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try?
+                JSONDecoder().decode(Response.self, from: data){
+                games = decodedResponse.games
+            }
+        }catch{
+            print("Invalid data")
+        }
     }
 }
 
